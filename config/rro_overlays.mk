15e616b06ad4046e7a177aaff84607620fd22e66
SagarMakhar <sagarmakhar@gmail.com>
2022-01-08 18:49:07 +0000

    vendor: Add Black theme [3/3]
    Change-Id: I362c75a9a9456bb4d7ffe12072b1b2ef1ea49dac
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -27,4 +27,5 @@ PRODUCT_PACKAGES += \
     GoogleConfigOverlay \
     SystemUIGoogleOverlay \
     StorageManagerGoogleOverlay \
-    FlipendoOverlay
+    FlipendoOverlay \
+    AndroidBlackThemeOverlay
