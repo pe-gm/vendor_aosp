27744aa1c20815a0b1724a9c3d801fb4fe83361c
Gustavo Mendes <gusttavo.me@outlook.com>
2022-09-15 13:21:24 -0300

    Just Kei!
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -2,11 +2,7 @@
 ifeq ($(BUILD_FINGERPRINT),)
 BUILD_NUMBER_CUSTOM := $(shell date -u +%H%M)
 CUSTOM_DEVICE ?= $(TARGET_DEVICE)
-ifneq ($(filter OFFICIAL,$(CUSTOM_BUILD_TYPE)),)
 BUILD_SIGNATURE_KEYS := release-keys
-else
-BUILD_SIGNATURE_KEYS := test-keys
-endif
 BUILD_FINGERPRINT := $(PRODUCT_BRAND)/$(CUSTOM_DEVICE)/$(CUSTOM_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER_CUSTOM):$(TARGET_BUILD_VARIANT)/$(BUILD_SIGNATURE_KEYS)
 endif
 ADDITIONAL_SYSTEM_PROPERTIES  += \
@@ -18,14 +14,10 @@ ADDITIONAL_SYSTEM_PROPERTIES  += \
     persist.sys.recovery_update=true
 endif
 
-# Custom security patch
-CUSTOM_SECURITY_PATCH := 2022-08-05
-
 # Versioning props
 ADDITIONAL_SYSTEM_PROPERTIES  += \
     org.pixelexperience.version=$(CUSTOM_VERSION_PROP) \
     org.pixelexperience.version.display=$(CUSTOM_VERSION) \
     org.pixelexperience.build_date=$(CUSTOM_BUILD_DATE) \
     org.pixelexperience.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
-    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE) \
-    org.pixelexperience.build_security_patch=$(CUSTOM_SECURITY_PATCH)
+    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE)
