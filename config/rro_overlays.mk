4fac7bda51d0b8ee4f4d888ba30e7e4db4abd145
Gustavo Mendes <gusttavo.me@outlook.com>
2023-01-23 11:42:25 -0300

    rro_overlays: Sort packages list
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -1,31 +1,31 @@
 # RRO Overlays
 PRODUCT_PACKAGES += \
-    NetworkStackOverlay \
-    PixelLauncherCustomOverlay \
-    PixelDocumentsUIGoogleOverlay \
     BuiltInPrintServiceOverlay \
-    GooglePermissionControllerOverlay \
-    PixelConfigOverlayCommon \
-    TeleServiceOverlay \
     CaptivePortalLoginOverlay \
-    GoogleWebViewOverlay \
-    TelecomOverlay \
     CellBroadcastServiceOverlay \
-    SettingsGoogleOverlay \
-    SettingsGoogleOverlayPixel2022 \
-    TelephonyProviderOverlay \
     ContactsProviderOverlay \
+    FlipendoOverlay \
+    GoogleConfigOverlay \
+    GooglePermissionControllerOverlay \
+    GoogleWebViewOverlay \
+    NetworkStackOverlay \
     PixelConfigOverlay2018 \
-    SettingsProviderOverlay \
-    TraceurOverlay \
     PixelConfigOverlay2019 \
     PixelConfigOverlay2019Midyear \
     PixelConfigOverlay2021 \
+    PixelConfigOverlayCommon \
+    PixelDocumentsUIGoogleOverlay \
+    PixelFwResOverlay \
+    PixelLauncherCustomOverlay \
     PixelSetupWizardOverlay \
     PixelSetupWizardOverlay2019 \
     PixelSetupWizardOverlay2021 \
-    PixelFwResOverlay \
-    GoogleConfigOverlay \
-    SystemUIGoogleOverlay \
+    SettingsGoogleOverlay \
+    SettingsGoogleOverlayPixel2022 \
+    SettingsProviderOverlay \
     StorageManagerGoogleOverlay \
-    FlipendoOverlay
+    SystemUIGoogleOverlay \
+    TeleServiceOverlay \
+    TelecomOverlay \
+    TelephonyProviderOverlay \
+    TraceurOverlay
